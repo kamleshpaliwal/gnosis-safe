b1a08d3884844b50da67ea0ce8553d4a9dae989b
Kamlesh Paliwal <kamlesh@whitehatjr.com>
2022-01-13 16:00:28 +0530

    corrected spelling mistakes

--- app/routes/transaction_routes.js
+++ app/routes/transaction_routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
     const transactionController = require('../controllers/transaction_controller.js')
     let router = require('express').Router()
       // Retrieve a balance with address id
-    router.get('/adresses', transactionController.findMultiple)
-    router.get('/adresses/:id', transactionController.findOne)
+    router.get('/addresses', transactionController.findMultiple)
+    router.get('/addresses/:id', transactionController.findOne)
     app.use('/api/balance', router)
   }
